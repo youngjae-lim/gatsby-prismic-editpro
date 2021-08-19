dd63c9cfe87b3dce56539296c21a01dcc941e629
Youngjae Lim <limyoungjae@gmail.com>
2021-08-19 00:42:26 -0400

    converted export default to module.exports = in the gatsby-config.js file to fix deployment issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
 
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 
-export default {
+module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
